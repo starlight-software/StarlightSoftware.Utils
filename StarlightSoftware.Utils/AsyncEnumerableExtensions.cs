2eab5004872250f3533f6f9d019ca34fc1f4b4ac
Joshua Moon <joshua.moon@bliss-systems.co.uk>
2022-12-13 13:47:43 +0000

    Add asyncenumerable and icon

---
+++ StarlightSoftware.Utils/AsyncEnumerableExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using System;
+
+namespace StarlightSoftware.Utils;
+
+public static class AsyncEnumerableExtensions
+{
+    public static async Task<List<T>> ToListAsync<T>(this IAsyncEnumerable<T> source)
+    {
+        // ReSharper disable PossibleMultipleEnumeration
+        if(source is null) throw new ArgumentNullException(nameof(source));
+
+        List<T> list = new();
+
+        await foreach (T element in source)
+        {
+            list.Add(element);
+        }
+
+        return list;
+        // ReSharper restore PossibleMultipleEnumeration
+    }
+}
